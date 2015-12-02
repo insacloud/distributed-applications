dc178e3ff9ba728cd4468b57541d5305f5203dd7
Mathieu Thoretton <mathieu@thoretton.com>
2015-12-02 11:14:04 +0100

    Init

---
+++ Vagrantfile
@@ -0,0 +1,14 @@
+Vagrant.configure(2) do |config|
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+
+  config.vm.provision "shell", path: "provision.sh"
+
+  # Forward des connexions ssh
+  config.ssh.forward_agent = true
+
+  config.vm.provider "virtualbox" do |v|
+    v.name   = "distributed-applications"
+    v.memory = 2048
+  end
+end
---
+++ provision.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+REPO_ROOT="/vagrant"
+USER="vagrant"
+
+# Update
+apt-get update -y
+apt-get upgrade -y
+# Install required packages
+apt-get install python-pip python-dev -y
+pip install -Iv ansible==1.9.3
+# set permissions
+for i in `find . -type f \( -name "hosts" \)`; do    sed -i 's/\r//' $i && chmod +x $i ; done
+
+# ssh key
+mkdir -p /root/.ssh/
+su $USER <<EOF
+ssh-keygen -t rsa -N "" -f "/home/$USER/.ssh/id_rsa"
+EOF
+cat "/home/$USER/.ssh/id_rsa.pub" | tee -a /root/.ssh/authorized_keys
+su $USER <<EOF
+ssh-keyscan localhost > ~/.ssh/known_hosts
+EOF
+
+# Command to deploy
+for d in $REPO_ROOT/inventories/*
+do
+    target=`basename $d`
+    read -r -d '' DEPLOY <<EOF
+#!/bin/bash
+cd "$REPO_ROOT" && ansible-playbook -i "$REPO_ROOT/inventories/$target" site.yml
+EOF
+    echo "$DEPLOY" | tee "/usr/local/bin/deploy_$target" > /dev/null
+    chmod +x "/usr/local/bin/deploy_$target"
+done
